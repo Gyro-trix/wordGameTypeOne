a2d4ca457acaa97228e90d4d44af1106fe70bd3c
Matthew Garrett <m.garrett@hotmail.ca>
2023-08-02 10:15:42 -0230

    Experimenting with timer modification

--- wordgame.js
+++ wordgame.js
@@ -1,22 +1,43 @@
+var score = 0;
+var val = 0;
+var gameState = 0; //Prevents starting of a new timer
+var timerMsg = "00:00";
+var timer;
 
+//Starts Score displayed as Zero
+document.getElementById("score").textContent = score;
+//Starts timer at zero
+document.getElementById("timer").textContent = timerMsg;
 
 function beginClick(){
-    new basicTimer(function(val) {
-        var timerMsg = "00:" + (val >= 10 ? val : "0" + val);
-        document.getElementById("timer").textContent = timerMsg; 
-    });
+    if (gameState == 0){
+        new basicTimer(function(val) {
+            timerMsg = "00:" + (val >= 10 ? val : "0" + val);
+            document.getElementById("timer").textContent = timerMsg; 
+        });
+        gameState = 1; 
+    }
 }
 
+function scoreUp(){
+    score = score + 10;
+    document.getElementById("score").innerHTML = "";
+    document.getElementById("score").textContent = score;
+}
 
 function basicTimer(callback, val) {
     val = val || 60; 
-    var timer=setInterval(function() { 
+    timer=setInterval(function() { 
         callback(val);
         if(val-- <= 0) { 
-            clearInterval(timer); 
+            clearInterval(timer); //Stops timer at zero
         } 
     }, 1000);
 }
+
+
+
+
 
 // Places Timer in the "id" portion of the html 
 
