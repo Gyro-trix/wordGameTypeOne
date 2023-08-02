777132777ec843dadeec9127b8c0f6195fce2e0a
Matthew Garrett <m.garrett@hotmail.ca>
2023-08-02 13:06:06 -0230

    Simpler timer for testing

--- wordgame.js
+++ wordgame.js
@@ -1,49 +1,25 @@
 var score = 0;
-var val = 0;
-var gameState = 0; //Prevents starting of a new timer
-var timerMsg = "00:00";
-var timer;
+var sec = 60;
 
-//Starts Score displayed as Zero
 document.getElementById("score").textContent = score;
-//Starts timer at zero
-document.getElementById("timer").textContent = timerMsg;
 
-function beginClick(){
-    if (gameState == 0){
-        new basicTimer(function(val) {
-            timerMsg = "00:" + (val >= 10 ? val : "0" + val);
-            document.getElementById("timer").textContent = timerMsg; 
-        });
-        gameState = 1;
-    } else if(gameState == 1){
+function timer(){
+    var timer = setInterval(function(){
+        document.getElementById('timer').innerHTML='00:'+sec;
+        sec--;
+        if (sec < 0) {
             clearInterval(timer);
-            gameState = 0;
-        } 
-    }
-
+        }
+    }, 1000);
+}
 
 function scoreUp(){
     score = score + 10;
+    sec = sec + 10;
     document.getElementById("score").innerHTML = "";
     document.getElementById("score").textContent = score;
 }
 
-function basicTimer(callback, val) {
-    val = val || 60; 
-    timer=setInterval(function() { 
-        callback(val);
-        if(val-- <= 0) { 
-            clearInterval(timer); //Stops timer at zero
-        } 
-    }, 1000);
-}
-
-
-
-
-
-// Places Timer in the "id" portion of the html 
 
 
 
