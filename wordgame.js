709e4e7537519aeb1b5f90676c5ee91059293021
Matthew Garrett <m.garrett@hotmail.ca>
2023-08-02 13:41:33 -0230

    Play Pause Button

--- wordgame.js
+++ wordgame.js
@@ -7,15 +7,18 @@ document.getElementById("score").textContent = score;
 
 function timer(){
     if (mode == 0){
-    time = setInterval(function(){
+        time = setInterval(function(){
         document.getElementById('timer').innerHTML='00:'+sec;
         sec--;
         if (sec < 0) {
             clearInterval(time);
         }
     }, 1000);
+    playPause();
     mode = 1;
+    
 } else{
+    playPause();
     mode = 0;
     clearInterval(time);
     
@@ -35,5 +38,13 @@ function restart(){
     location.reload();
 }
 
+function playPause(){
+    if (mode == 0){
+        const button = document.getElementById("play").innerHTML = "Pause";
+    } 
+    if (mode == 1){
+        const button = document.getElementById("play").innerHTML = "Play";
+    }
+    }
 
 
